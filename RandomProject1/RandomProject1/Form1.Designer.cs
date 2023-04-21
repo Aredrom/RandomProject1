9a4eb742ae49ccd696dd9f488707f8dac58681ee
Aredrom <sadaos2.97@gmail.com>
2023-04-21 16:02:17 +0200

    Adding UI components

--- RandomProject1/RandomProject1/Form1.Designer.cs
+++ RandomProject1/RandomProject1/Form1.Designer.cs
@@ -28,12 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            comboBoxContinent = new ComboBox();
+            numericUpDown1 = new NumericUpDown();
+            GetCountriesBtn = new Button();
+            listBox1 = new ListBox();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // comboBoxContinent
+            // 
+            comboBoxContinent.FormattingEnabled = true;
+            comboBoxContinent.Location = new Point(12, 12);
+            comboBoxContinent.Name = "comboBoxContinent";
+            comboBoxContinent.Size = new Size(121, 23);
+            comboBoxContinent.TabIndex = 0;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(13, 41);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 1;
+            // 
+            // GetCountriesBtn
+            // 
+            GetCountriesBtn.Location = new Point(58, 70);
+            GetCountriesBtn.Name = "GetCountriesBtn";
+            GetCountriesBtn.Size = new Size(75, 23);
+            GetCountriesBtn.TabIndex = 2;
+            GetCountriesBtn.Text = "Show";
+            GetCountriesBtn.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(223, 12);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(565, 424);
+            listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBox1);
+            Controls.Add(GetCountriesBtn);
+            Controls.Add(numericUpDown1);
+            Controls.Add(comboBoxContinent);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ComboBox comboBoxContinent;
+        private NumericUpDown numericUpDown1;
+        private Button GetCountriesBtn;
+        private ListBox listBox1;
     }
 }
--- RandomProject1/RandomProject1/Form1.cs
+++ RandomProject1/RandomProject1/Form1.cs
@@ -2,9 +2,27 @@ namespace RandomProject1
 {
     public partial class Form1 : Form
     {
+        Dictionary<string, string> continentDictionary = new Dictionary<string, string>
+        {
+            { "AF", "Africa" },
+            { "AN", "Antarctica" },
+            { "AS", "Asia" },
+            { "EU", "Europe" },
+            { "NA", "North America" },
+            { "OC", "Oceania" },
+            { "SA", "South America" }
+        };
+
         public Form1()
         {
             InitializeComponent();
+
+            comboBoxContinent.DataSource = new BindingSource(continentDictionary, null);
+            comboBoxContinent.DisplayMember = "Value";
+            comboBoxContinent.ValueMember = "Key";
+
+            numericUpDown1.Minimum = 2;
+            numericUpDown1.Maximum = 10;
         }
     }
 }
