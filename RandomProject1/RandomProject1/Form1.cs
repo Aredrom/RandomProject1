5b021aa9f7e57761696c53728393c61cc9d5bc67
Aredrom <sadaos2.97@gmail.com>
2023-04-21 17:34:00 +0200

    Consuming other api and displaying info to user

--- RandomProject1/RandomProject1/Form1.cs
+++ RandomProject1/RandomProject1/Form1.cs
@@ -2,7 +2,9 @@ using GraphQL;
 using GraphQL.Client.Http;
 using GraphQL.Client.Serializer.SystemTextJson;
 using RandomProject1.Models;
+using System.Net.Http.Json;
 using static RandomProject1.Models.GraphQLContinentsResponse;
+using static RandomProject1.Models.RestApiResponse;
 
 namespace RandomProject1
 {
@@ -53,7 +55,42 @@ namespace RandomProject1
             var graphQLResponse = await graphQLClient.SendQueryAsync<ContinentResponse>(graphQLRequest);
             var countries = graphQLResponse.Data.Continent.Countries;
 
-            listBox1.Items.Add(countries.Count);
+            var random = new Random();
+            var selectedCountries = countries.OrderBy(x => random.Next()).Take((int)numericUpDown1.Value);
+
+            foreach (var country in selectedCountries)
+            {
+                string countryName = country.Name;
+                string apiUrl = $"https://restcountries.com/v3.1/name/{countryName}";
+                var allCountryInfo = new List<CountryInfo>();
+
+                var apiClient = new HttpClient();
+                var apiResponse = await apiClient.GetAsync(apiUrl);
+                if (apiResponse.IsSuccessStatusCode)
+                {
+                    var content = await apiResponse.Content.ReadFromJsonAsync<List<CountryInfo>>();
+                    foreach (var data in content)
+                    {
+                        allCountryInfo.Add(data);
+                    }
+                }
+                else
+                {
+                    listBox1.Text = $"No country information found for '{country.Name}'.";
+                }
+
+                var sortedCountries = allCountryInfo.OrderByDescending(x => x.OfficialName);
+                foreach (var sortedCountry in sortedCountries)
+                {
+                    listBox1.Items.Add($"Official name: {sortedCountry.OfficialName}");
+                    listBox1.Items.Add($"Capital: {sortedCountry.Capital.First()}");
+                    listBox1.Items.Add($"Population: {sortedCountry.Population}");
+                    listBox1.Items.Add($"Currency: {sortedCountry.Currency}");
+                    listBox1.Items.Add($"Subregion: {sortedCountry.Subregion}");
+                    listBox1.Items.Add($"Languages: {sortedCountry.Languages}");
+                    listBox1.Items.Add("");
+                }
+            }
         }
     }
 }
---
+++ RandomProject1/RandomProject1/Models/RestApiResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace RandomProject1.Models
+{
+    public class RestApiResponse
+    {
+        public class CountryInfo
+        {
+            public string OfficialName { get; set; }
+            public List<string> Capital { get; set; }
+            public int Population { get; set; }
+            public string Currency { get; set; }
+            public string Subregion { get; set; }
+            public Dictionary<string, string> Languages { get; set; }
+        }
+    }
+}
